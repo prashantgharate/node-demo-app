357dbb9225a4ebbbee093d57a807c03806687785
Ubuntu <ubuntu@ip-172-31-41-76.ap-south-1.compute.internal>
2025-08-04 14:05:50 +0000

    all

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Prashant!');
+  res.send('Welcome from Prashant Gharate Created Nodejs App!');
 });
 
 app.listen(3000, () => {
