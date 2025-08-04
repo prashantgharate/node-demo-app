d4fc4e7011730df11b2a7dca2280d99a72037fa7
Ubuntu <ubuntu@ip-172-31-41-76.ap-south-1.compute.internal>
2025-08-04 08:39:45 +0000

    new

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
